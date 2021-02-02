84e2a87359f80016ceb693069dac8c16f60c7e29
laariche <kawtarlaariche94@gmail.com>
2021-02-02 18:28:54 +0100

    initialize project structure
    add core and shared modules; add base http service

---
+++ src/app/core/http/base.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { environment } from '@env';
+import { HttpClient, HttpErrorResponse, HttpParams, HttpResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class BaseService<T> {
+
+  protected url = '';
+
+  constructor(protected http: HttpClient) { }
+
+  protected findAll(): Observable<T[]> {
+      return this.http.get(this.url)
+                      .pipe(
+                        map(res => res as T[]),
+                        catchError(this.errorHandeler));
+  }
+
+  protected findById(id:number): Observable<T>{
+    let params = new HttpParams().set("id", id.toString());
+
+    return this.http.get(this.url, {params: params})
+                      .pipe(
+                        map(res => res as T),
+                        catchError(this.errorHandeler));
+  }
+
+  protected create(model:T): Observable<T>{
+   return this.http.post(this.url, model)
+                    .pipe(
+                      map(res => res as T),
+                      catchError(this.errorHandeler));
+  }
+
+  protected update(id:number, data:T): Observable<any>{
+    let params = new HttpParams().set("id", id.toString());
+
+    return this.http.put(this.url, { params: params }, data)
+                      .pipe(
+                        map(res => {}),
+                        catchError(this.errorHandeler));
+  }
+
+  protected delete(id:number): Observable<any>{
+    let params = new HttpParams().set("id", id.toString());
+
+    return this.http.delete(this.url,{ params: params })
+                      .pipe(
+                        map(res => {}),
+                        catchError(this.errorHandeler));
+  }
+
+  protected errorHandeler(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+ }
+
+}
---
+++ src/app/core/http/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { BaseService } from './base.service';
+import { User } from '@shared/models/user';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService extends BaseService<User>{
+
+  constructor(protected http: HttpClient) {
+    super(http);
+  }
+}
